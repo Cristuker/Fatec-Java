5526f0713a73c79a1c21d6416525ee7fe2199e19
Cristian <cristian123105@gmail.com>
2020-09-15 20:44:13 -0300

    add aula TCP -ES3

---
+++ ES3-Projetos/ambienteDeRede/src/main/java/com/mycompany/ambientederede/ClienteTCP.java
@@ -0,0 +1,22 @@
+package com.mycompany.ambientederede;
+
+import java.net.*;
+import java.io.*;
+
+
+public class ClienteTCP {
+    public static void main(String[] args) {
+        
+        String enderecoServidor = "127.0.0.1";
+        int portaServidor = 12258;
+        
+        try{
+            Socket conexao = new Socket(enderecoServidor, portaServidor);
+            DataInputStream entrada = new DataInputStream(conexao.getInputStream());
+            System.out.println("Número recebido: "+ entrada.readInt());
+            conexao.close();
+        }catch(Exception error){
+            System.out.println(error.toString());
+        }
+    }
+}
---
+++ ES3-Projetos/ambienteDeRede/src/main/java/com/mycompany/ambientederede/ServidorTCP.java
@@ -0,0 +1,32 @@
+package com.mycompany.ambientederede;
+
+import java.net.*;
+import java.io.*;
+
+
+public class ServidorTCP {
+    public static void main(String[] args) {
+
+        //Acima de 1023
+        int porta = 12258;
+
+        try {
+            //Numero de clientes que pode ser atendido na porta
+            ServerSocket servidor = new ServerSocket(porta,2);
+            System.out.println("Esperando conexão...");
+            Socket conexao = servidor.accept(); //Servidor foi criado e ele esta esperando algum cliente se conectar
+            
+
+            DataOutputStream saida = new DataOutputStream(conexao.getOutputStream());// Por onde o numero sera enviado
+            SocketAddress s = conexao.getRemoteSocketAddress(); // Pegando o endereço do cliente
+            System.out.println("Enviando número ao cliente: "+s.toString()); 
+            saida.writeInt(12);
+            conexao.close();
+
+        }catch(Exception error){
+            System.out.println(error.toString());
+        }
+        
+    }
+     
+}
